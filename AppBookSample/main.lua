0cb0c37a5c550cf988bb8e3803b83ae15576adb9
reiui1234 <reiui1234@gmail.com>
2016-06-14 23:13:48 +0900

    주석추가, 신템플릿 파일 추가

--- AppBookSample/main.lua
+++ AppBookSample/main.lua
@@ -1,5 +1,5 @@
 display.setStatusBar( display.HiddenStatusBar ) -- 상태바를 숨긴다.
 
-local composer = require( "composer" )
+local composer = require( "composer" ) -- 컴포져 모듈 임포트
 
-composer.gotoScene( "sc_intro" )
+composer.gotoScene( "sc_intro" ) -- 인트로 화면으로 이동
