51c8da9a38bc6fb0f487742ce040e83ed9941007
KSH <reiui1234@gmail.com>
2016-06-16 08:54:41 +0900

    주석추가

--- AppBookSample/sc_intro.lua
+++ AppBookSample/sc_intro.lua
@@ -9,11 +9,13 @@ local img_bg, img_cona
 function scene:create( event )
     local sceneGroup = self.view    
 
+    --흰색 배경 생성
     img_bg       = display.newRect( display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight )
     img_bg.alpha = 0
     img_bg:setFillColor( 1, 1, 1 )
     sceneGroup:insert( img_bg )
 
+    -- 코나 캐릭터 생성
     img_cona       = display.newImage( "images/img_cona.png" )
     img_cona.x     = display.contentCenterX
     img_cona.y     = display.contentCenterY
@@ -31,9 +33,11 @@ function scene:show( event )
         
     elseif ( phase == "did" ) then       
 
+        --Fade in 효과
         transition.to( img_bg, { delay = 1000, time = 1000, alpha = 1  } )
         transition.to( img_cona, { delay = 1000, time = 1000, alpha = 1  } )
 
+        --Fade out 효과
         transition.to( img_bg, { delay = 4000, time = 1000, alpha = 0  } )
         transition.to( img_cona, { delay = 4000, time = 1000, alpha = 0  } )
 
@@ -46,6 +50,7 @@ function scene:show( event )
             
         end
 
+        --5초 되에 goMainMenu 실행
         timer.performWithDelay( 5000, goMainMenu, 1 )
     end
 end
